507db2aa178166ab96a22cdd98425a0a7713d85d
Walter Barbieri <walter90@hotmail.it>
2023-09-18 21:43:21 +0200

    login completato e grafica quasi completata

--- EcommerceGioielleriaAngular/eCommerce/src/app/app.module.ts
+++ EcommerceGioielleriaAngular/eCommerce/src/app/app.module.ts
@@ -20,8 +20,11 @@ const route: Route[] = [
     {
         path: 'register',
         component: RegisterComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
-
 ]
 @NgModule({
   declarations: [
--- EcommerceGioielleriaAngular/eCommerce/src/app/auth/login/login/login.component.ts
+++ EcommerceGioielleriaAngular/eCommerce/src/app/auth/login/login/login.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { catchError, of } from 'rxjs';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+    error!: string;
+    showAlert = true;
 
-  constructor() { }
+    constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    accedi(form: NgForm) {
+        console.log(form.value);
+
+        this.authService
+            .login(form.value)
+            .pipe(
+                catchError((error) => {
+                    console.error(error);
+                    this.error = error;
+                    form.reset;
+                    return of(null);
+                })
+            )
+            .subscribe((response) => {
+                if (response) {
+                    sessionStorage.setItem('isFirstLoad', 'true');
+                    this.router.navigate(['/']);
+                }
+            });
+    }
+    redirectToRegister() {
+        this.router.navigate(['/register']);
+      }
+
+      chiudiAlert(){
+        this.showAlert = false;
+        this.error = '';
+        this.showAlert = true;
+      }
 }
